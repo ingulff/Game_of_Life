192946a33d72f47f316ed4d7640c1b126a21dc92
Константин Геворкян <konstantin.gevorkyan.personal@gmail.com>
2024-03-14 20:38:05 +0500

    Add basic project files for minimal build
    Add doctest for unit tests
    Add minimal project files for build with sdl

---
+++ source/main.cpp
@@ -0,0 +1,16 @@
+/**
+ *  ᛝ
+ */
+
+#include <iostream> // delete this
+
+#include <SDL.h>
+
+#include "main_window.hpp"
+
+int main(int argc, char ** argv)
+{
+	tt_program::main_window main_window;
+
+	return main_window.exec();
+}
---
+++ source/main_window.hpp
@@ -0,0 +1,42 @@
+#ifndef TT_GAME_OF_LIFE_HPP
+#define TT_GAME_OF_LIFE_HPP
+
+#include <memory>
+
+namespace tt_program
+{
+
+class main_window
+{
+
+public:
+	enum class status_code
+	{
+		normal,
+		invalid,
+	};
+
+public:
+	main_window();
+
+	main_window(main_window & other) = delete;
+	main_window & operator=(main_window & other) = delete;
+
+	main_window(main_window && other);
+	main_window & operator=(main_window && other);
+
+	~main_window();
+
+public:
+	int exec();
+
+private:
+	class main_window_impl;
+	std::unique_ptr<main_window_impl> m_impl;
+	enum class status_code m_status;
+};
+
+} // namespace tt_program
+
+
+#endif // TT_GAME_OF_LIFE_HPP
