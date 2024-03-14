f167bfc60e2e959964c21e02afedf49351030045
Константин Геворкян <konstantin.gevorkyan.personal@gmail.com>
2024-03-15 00:16:32 +0500

    Move error status to errors file

---
+++ source/error/error.hpp
@@ -0,0 +1,20 @@
+#ifndef TT_ERROR_HPP
+#define TT_ERROR_HPP
+
+namespace error
+{
+
+enum class status_code
+{
+	normal,
+	invalid,
+};
+
+enum class errc
+{
+	OK,
+};
+
+} // namespace error
+
+#endif // TT_ERROR_HPP
