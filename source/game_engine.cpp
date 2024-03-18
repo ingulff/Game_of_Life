b39a1f039be4accb00b4133068dda964b29361f1
Константин Геворкян <konstantin.gevorkyan.personal@gmail.com>
2024-03-19 00:49:06 +0500

    Add calc next step of game

--- source/game_engine.cpp
+++ source/game_engine.cpp
@@ -3,7 +3,10 @@
  */
 
 #include <iostream>
+#include <fstream>
+
 #include <memory>
+#include <set>
 #include <utility>
 
 #include <cstdint>
@@ -46,18 +49,136 @@ public:
 	}
 
 public:
-	std::shared_ptr<std::int8_t[]> update()
+	std::shared_ptr<std::int8_t[]> update(error::status_code & status)
 	{
-		//calculate_next_step();
+		if( is_playable(status) )
+		{
+			clear_reserve_board(m_reserve_board);
+			calculate_next_step();
 
-		//swap(m_board, m_reserve_board);
+			swap(m_board, m_reserve_board);
+		}
 
 		return m_board;
+	}
+
+public:
+	bool is_playable(error::status_code & status)
+	{
+		return status == error::status_code::active;
+	}
+
+	void clear_reserve_board(board_t & reserve_board)
+	{
+		for(std::int32_t i = 0, size = ((m_board_width * m_board_height) / 20 + 1) / 8 + 1;
+			i < size; ++i)
+		{
+			reserve_board[i] = 0;
+		}
 	}
 
 private:
 	void calculate_next_step()
 	{
+		using tt_program::details::index_by_coordinates;
+		using tt_program::details::bit_index_by_coordinates;
+		using tt_program::details::coordinates_by_index;
+
+		//std::int32_t block_index = start_pos;
+		std::int32_t board_size = ((1000 / 20 ) * (1000 / 20)) / 8 ;
+		// находим блок, где есть изменненный бит, бит == селл
+		for(std::int32_t cell_group_index = 0; 
+			cell_group_index < board_size; 
+			++cell_group_index)
+		{
+			for(std::int8_t bit_index = 0; bit_index < 8; ++bit_index)
+			{
+				auto [x, y] = coordinates_by_index(cell_group_index, bit_index );
+				auto neighbors = calclulate_neighbors(x, y);
+				if( need_cell_alive(x, y, neighbors))
+				{
+
+					m_reserve_board[index_by_coordinates(x, y)] |= (1 << bit_index_by_coordinates(x, y));
+				}
+				else
+				{
+					m_reserve_board[index_by_coordinates(x, y)] &= ~(1 << bit_index_by_coordinates(x, y));
+				}
+			}
+		}
+	}
+
+private:
+
+	bool need_cell_alive(std::int32_t x, std::int32_t y, std::int32_t neighbors)
+	{
+		using tt_program::details::index_by_coordinates;
+		using tt_program::details::bit_index_by_coordinates;
+ 
+		return ( (neighbors == 3) && !( (m_board[index_by_coordinates(x, y)] >> bit_index_by_coordinates(x, y))  & 1 ) ) 
+			|| ( (neighbors == 2 || neighbors == 3) && ( (m_board[index_by_coordinates(x, y)] >> bit_index_by_coordinates(x, y))  & 1 ) );;
+ 	}
+
+
+	std::int8_t calclulate_neighbors(std::int32_t x, std::int32_t y)
+	{
+		using tt_program::details::index_by_coordinates;
+		using tt_program::details::bit_index_by_coordinates;
+
+		std::int8_t neighbors = 0;
+		bool has_prev_x = (x - 1 > -1);
+		bool has_next_x = (x + 1 < m_board_width / 20);
+		if(y - 1 > -1)
+		{
+			if( has_prev_x )
+			{
+				neighbors += is_cell_alive(x-1, y-1);
+			}
+
+			neighbors += is_cell_alive(x, y-1);
+
+			if( has_next_x )
+			{
+				neighbors += is_cell_alive(x+1, y-1);
+			}
+		}
+
+		if(y + 1 < m_board_height / 20)
+		{
+			if( has_prev_x )
+			{
+				neighbors += is_cell_alive(x-1, y+1);
+			}
+
+			neighbors += is_cell_alive(x, y+1);
+
+			if( has_next_x )
+			{
+				neighbors += is_cell_alive(x+1, y+1);
+			}
+		}
+
+		if( has_prev_x )
+		{
+			neighbors += is_cell_alive(x-1, y);
+		}
+
+		if( has_next_x )
+		{
+			neighbors += is_cell_alive(x+1, y);
+		}
+
+		return neighbors;
+	}
+
+	bool is_cell_alive(std::int32_t x, std::int32_t y)
+	{
+		using tt_program::details::index_by_coordinates;
+		using tt_program::details::bit_index_by_coordinates;
+
+		bool is_alive = ( ( m_board[index_by_coordinates(x, y)] >> bit_index_by_coordinates(x, y) )  & 1 );
+ 
+		return  ( ( m_board[index_by_coordinates(x, y)] >> bit_index_by_coordinates(x, y) )  & 1 );
 
 	}
 
@@ -81,6 +202,9 @@ private:
 		using tt_program::details::index_by_coordinates;
 		using tt_program::details::bit_index_by_coordinates;
 
+		auto index = index_by_coordinates(x, y);
+		auto bit = bit_index_by_coordinates(x, y);
+
 		m_board[index_by_coordinates(x, y)] |= (1 << bit_index_by_coordinates(x, y));
 	}
 
@@ -89,6 +213,9 @@ private:
 		using tt_program::details::index_by_coordinates;
 		using tt_program::details::bit_index_by_coordinates;
 		
+		auto index = index_by_coordinates(x, y);
+		auto bit = bit_index_by_coordinates(x, y);
+
 		m_board[index_by_coordinates(x, y)] &= ~(1 << bit_index_by_coordinates(x, y));
 	}
 
@@ -97,6 +224,7 @@ private:
 	std::int32_t m_board_height;
 	board_t m_board;
 	board_t m_reserve_board;
+	std::ofstream m_file;
 };
 
 
@@ -117,9 +245,9 @@ enum class error::status_code engine::initialize(std::int32_t width, std::int32_
 }
 
 
-std::shared_ptr<std::int8_t[]> engine::update()
+std::shared_ptr<std::int8_t[]> engine::update(error::status_code & status)
 {
-	return m_impl->update();
+	return m_impl->update(status);
 }
 
 
